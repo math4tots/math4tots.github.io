03b191e99abd262586784d26c72ff97e49422ef6
math4tots <math4tots@users.noreply.github.com>
2023-11-05 03:39:02 +0700

    /thai/ fix broken serviceworker

--- thai/serviceworker.js
+++ thai/serviceworker.js
@@ -26,30 +26,8 @@ self.addEventListener("activate", event => {
 });
 
 self.addEventListener("fetch", event => {
-  // // "Network first" strategy as described in
-  // // https://web.dev/learn/pwa/serving
-  // event.respondWith(fetch(event.request).catch(error =>
-  //   caches.match(event.request)));
-
-  // "Stale while revalidate" strategy as described in
+  // "Network first" strategy as described in
   // https://web.dev/learn/pwa/serving
-  self.addEventListener('fetch', event => {
-    event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
-        const networkFetch = fetch(event.request).then(response => {
-          // update the cache with a clone of the network response
-          const responseClone = response.clone();
-          caches.open(url.searchParams.get('name')).then(cache => {
-            cache.put(event.request, responseClone);
-          });
-          return response;
-        }).catch(function (reason) {
-          console.error('ServiceWorker fetch failed: ', reason);
-        });
-        // prioritize cached response over network
-        return cachedResponse || networkFetch;
-      }
-      )
-    );
-  });
+  event.respondWith(fetch(event.request).catch(error =>
+    caches.match(event.request)));
 });
